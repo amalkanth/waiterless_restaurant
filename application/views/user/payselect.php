84640901614046d1943853467eb7399fb432ecbe
vishnusn656 <vishnusn656@gmail.com>
2017-03-29 21:33:10 +0530

    paycardpage

---
+++ application/views/user/payselect.php
@@ -0,0 +1,4 @@
+<a href="../admin/billing" class="btn btn-success "style="float: center;">PAY BY CASH</a>
+<br><br>
+
+<a href="../user/paycard" class="btn btn-success "style="float: center;">PAY BY CARD</a>
